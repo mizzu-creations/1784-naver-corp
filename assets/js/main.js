0de98085cdb9eb23d45020d6c55eeac998885eb2
mizzu-creations <mizzu.creations@gmail.com>
2024-07-24 19:20:30 +0900

    Feat : flex slide 전체 스크롤 애니메이션 구현 및 프로젝트 완성

--- assets/js/main.js
+++ assets/js/main.js
@@ -197,266 +197,244 @@ function sectionInnovation() {
   });
 }
 function sectionMainSlide() {
-  const sideMarquee = gsap.to(".marquee-item", {
-    xPercent: 10,
-    repeat: -1,
-    duration: 4,
-    ease: "none",
-  });
-
-  sideMarquee.pause();
-
-  const tlSlide = gsap.timeline({
-    ease: "none",
-  });
-
-  let currentX1 = 0;
-  let currentX2 = 0;
-  let currentX3 = 0;
-
-  $(".flex.slide .sticky section").each((index, section) => {
-    const sectionWidth = $(section).width();
-    const slideWidth = $(".flex.slide .sticky").width();
-    const xPercent = (sectionWidth / slideWidth) * 100;
-
-    if (index === 0) {
-      currentX1 = xPercent;
-    } else if (index === 1) {
-      currentX2 = xPercent;
-    } else if (index === 2) {
-      currentX3 = xPercent;
-    }
-  });
+  const tlSlide = gsap.timeline({ duration: 0.5 });
 
   tlSlide
-    .set(".flex.slide .marquee", {
-      xPercent: -100,
-      onStart: () => {
-        $(".marquee-item .uppercase").text("tech");
-        $(".marquee").removeClass("dark");
+    .to(
+      ".flex.slide .sticky",
+      {
+        x: () => {
+          return -($(".sc-tech").width() / 2 - 70);
+        },
       },
-    })
+      "-=0.3"
+    )
+    .set(".tech-title", { autoAlpha: 0 }, "<")
+    .set(".slide-title", { autoAlpha: 1 }, "<")
     .to(".flex.slide .sticky", {
-      xPercent: -(currentX1 / 2),
-      x: 80,
-      delay: 0.1,
-    })
-    .set(".sc-tech .tech-title", { autoAlpha: 0 }, "<")
-    .to(".flex.slide .sticky", {
-      xPercent: -currentX1,
-      x: 280,
+      x: () => {
+        return -($(".sc-tech").width() - 280);
+      },
       delay: 0.1,
     })
     .set(".slide-title", { autoAlpha: 0 })
-    .set(
-      ".sc-tech .tech-title",
-      { display: "flex", justifyContent: "flex-end" },
-      "<"
-    )
+    .set(".tech-title", { justifyContent: "flex-end", autoAlpha: 1 }, "<")
     .to(".flex.slide .sticky", {
-      xPercent: -currentX1,
-      x: -300,
-    })
-    .to(".flex.slide .sticky", {
-      xPercent: -(currentX1 + currentX2),
-      x: 435,
-      duration: 2,
-      onStart: () => {
-        $(".slide-title").text("new normal");
-        $(".slide-title").css("color", "#000");
-        $(".marquee-item .uppercase").text("new normal");
-        $(".marquee").css(
-          "background-image",
-          "linear-gradient(0deg, #D6FE51, #68D840)"
-        );
-        $(".marquee").addClass("dark");
+      x: () => {
+        return -($(".sc-tech").width() + 300);
       },
+      delay: 0.1,
     })
+    .to(".flex.slide .sticky", {
+      x: () => {
+        return -($(".sc-tech").width() + $(".sc-newnormal").width() - 430);
+      },
+      delay: 0.1,
+    })
+    .set(".newnormal-title", { autoAlpha: 0 }, "<")
     .set(
       ".slide-title",
       {
         autoAlpha: 1,
-      },
-      "<"
-    )
-    .set(".sc-newnormal .newnormal-title", { autoAlpha: 0 }, "<")
-    .to(".flex.slide .sticky", {
-      xPercent: -(currentX1 + currentX2),
-      x: -580,
-    })
-    .set(".slide-title", { autoAlpha: 0 }, "<")
-    .set(
-      ".sc-newnormal .newnormal-title",
-      {
-        display: "flex",
-        justifyContent: "flex-end",
         color: "#000",
-        autoAlpha: 1,
+        text: {
+          value: "new normal",
+        },
       },
       "<"
     )
+    .set(".slide-title", { autoAlpha: 0 })
+    .set(".newnormal-title", { justifyContent: "flex-end", autoAlpha: 1 }, "<")
     .to(".flex.slide .sticky", {
-      xPercent: -(currentX1 + currentX2 + currentX3),
-      x: -400,
-      duration: 2,
-      onStart: () => {
-        $(".slide-title").text("eco");
-      },
-    })
-    .set(".slide-title", { autoAlpha: 1 }, "<")
-    .set(
-      ".sc-eco .eco-title",
-      {
-        autoAlpha: 0,
-      },
-      "<"
-    )
-    .to(".flex.slide .sticky", {
-      xPercent: -100,
       x: () => {
-        return window.innerWidth;
+        return -($(".sc-tech").width() + $(".sc-newnormal").width() + 610);
       },
-      onComplete: () => {
-        $(".flex.slide .sticky section > .title").css("position", "static");
-      },
+      delay: 0.1,
     })
-    .set(".slide-title", { autoAlpha: 0 }, "<")
+    .set(".eco-title", { autoAlpha: 0 })
     .set(
-      ".sc-eco .eco-title",
+      ".slide-title",
       {
-        display: "flex",
-        justifyContent: "flex-end",
-        color: "#000",
         autoAlpha: 1,
+        text: {
+          value: "eco",
+        },
       },
       "<"
-    );
-
-  // tlSlide
-  // .to(".flex.slide .sticky", {
-  //   xPercent: currentX,
-  //   duration: 1,
-  //   ease: "none",
-  //   onComplete: () => {
-  //     // 섹션에 도달했을 때 멈추기 위한 지연
-  //     gsap.to(".flex.slide .sticky", {
-  //       duration: 1,
-  //       xPercent: currentX,
-  //       ease: "none",
-  //       delay: 1 // 각 섹션에서 멈추는 시간 조정
-  //     });
-  //   }
-  // });
-  // .to(".flex.slide .sticky", {
-  //   xPercent: -25,
-  //   delay: 0.05,
-  // })
-  // .to(".flex.slide .sticky", {
-  //   xPercent: -50,
-  //   delay: 0.05,
-  // })
-  // .to(".flex.slide .sticky", {
-  //   xPercent: -75,
-  //   delay: 0.05,
-  // })
-  // .to(".flex.slide .sticky", {
-  //   xPercent: -100,
-  //   delay: 0.05,
-  // });
-  // .set(".sc-innovation", { autoAlpha: 0 });
+    )
+    .to(".flex.slide .sticky", {
+      x: () => {
+        return -(
+          $(".sc-tech").width() +
+          $(".sc-newnormal").width() +
+          $(".sc-eco").width() +
+          395
+        );
+      },
+      delay: 0.1,
+    })
+    .to(".flex.slide .sticky", {
+      x: () => {
+        return -($(".flex.slide .sticky").innerWidth() - window.innerWidth);
+      },
+      delay: 0.1,
+    })
+    .set(".slide-title", { autoAlpha: 0 }, "<")
+    .set(".eco-title", { justifyContent: "flex-end", autoAlpha: 1 }, "<")
+    .set(".sc-innovation", { autoAlpha: 0 }, "<");
 
   ScrollTrigger.create({
     trigger: ".flex.slide",
     start: "top top",
     end: "bottom bottom",
     animation: tlSlide,
-    invalidateOnRefresh: true,
-    // markers: true,
     scrub: 1,
     onEnter: () => {
-      setHeaderStyle(false);
-      animateMarquee("fixed");
+      setHeaderStyle("black");
+      $(".flex.slide > [class*='marquee-']").css("position", "fixed");
+      gsap.set(".flex.slide > [class*='marquee-']", {
+        autoAlpha: 0,
+        xPercent: -100,
+        onComplete: () => {
+          sideMarquee.play();
+          gsap.to(`.flex.slide .marquee-01`, { xPercent: 0, autoAlpha: 1 });
+        },
+      });
     },
     onLeaveBack: () => {
-      setHeaderStyle(true);
-      animateMarquee("static");
+      setHeaderStyle("transparent");
+      gsap.to(`.flex.slide .marquee-01`, {
+        xPercent: -100,
+        autoAlpha: 0,
+        onComplete: () => {
+          $(".flex.slide > [class*='marquee-']").css("position", "static");
+          sideMarquee.pause();
+        },
+      });
     },
     onEnterBack: () => {
-      animateMarquee("fixed");
+      $(".flex.slide > [class*='marquee-']").css("position", "fixed");
+      gsap.to(`.flex.slide > [class*='marquee-']`, {
+        xPercent: 0,
+        onComplete: () => {
+          sideMarquee.play();
+        },
+      });
     },
     onLeave: () => {
-      animateMarquee("static");
+      gsap.to(`.flex.slide > [class*='marquee-']`, {
+        xPercent: -100,
+        onComplete: () => {
+          $(".flex.slide > [class*='marquee-']").css("position", "static");
+          sideMarquee.pause();
+        },
+      });
     },
     onUpdate: ({ progress }) => {
+      const changeSideMarquee = (progress) => {
+        if (progress >= 0 && progress < 0.44) {
+          gsap.to(`.flex.slide .marquee-01`, {
+            xPercent: 0,
+            autoAlpha: 1,
+            duration: 0.2,
+          });
+        } else {
+          gsap.to(`.flex.slide .marquee-01`, { autoAlpha: 0, duration: 0.2 });
+        }
+
+        if (progress >= 0.44 && progress < 0.72) {
+          gsap.to(`.flex.slide .marquee-02`, {
+            xPercent: 0,
+            autoAlpha: 1,
+            duration: 0.2,
+          });
+        } else {
+          gsap.to(`.flex.slide .marquee-02`, { autoAlpha: 0, duration: 0.2 });
+        }
+
+        if (progress >= 0.72 && progress < 0.88) {
+          gsap.to(`.flex.slide .marquee-03`, {
+            xPercent: 0,
+            autoAlpha: 1,
+            duration: 0.2,
+          });
+        } else {
+          gsap.to(`.flex.slide .marquee-03`, { autoAlpha: 0, duration: 0.2 });
+        }
+
+        if (progress >= 0.88 && progress <= 1) {
+          gsap.to(`.flex.slide .marquee-04`, {
+            xPercent: 0,
+            autoAlpha: 1,
+            duration: 0.2,
+          });
+        } else {
+          gsap.to(`.flex.slide .marquee-04`, {
+            autoAlpha: 0,
+            duration: 0.2,
+          });
+        }
+      };
+      const changeHeaderStyle = (progress) => {
+        if (progress < 0.23) {
+          setHeaderStyle("black");
+        } else if (progress >= 0.23 && progress < 1) {
+          setHeaderStyle("white");
+        } else if (progress >= 1) {
+          setHeaderStyle("transparent");
+        }
+      };
       const updateBackgroundImage = (progress) => {
         let colorValue;
-        if (progress >= 0.25 && progress <= 0.45) {
-          colorValue = Math.floor((255 * (progress - 0.3)) / 0.1);
-        } else if (progress < 0.25) {
+
+        if (progress >= 0.2 && progress <= 0.52) {
+          colorValue = Math.floor((255 * (progress - 0.2)) / 0.25);
+        } else if (progress < 0.2) {
           colorValue = 0;
         } else {
           colorValue = 255;
         }
+
+        colorValue = Math.min(255, Math.max(0, colorValue));
 
         $(".flex.slide .sticky").css(
           "background-image",
           `linear-gradient(to right, rgb(${colorValue}, ${colorValue}, ${colorValue}), rgb(${colorValue}, ${colorValue}, ${colorValue}))`
         );
       };
+
+      changeSideMarquee(progress);
       updateBackgroundImage(progress);
-
-      // 추후 autoAlpha로 개선
-      // if (progress < 0.36) {
-
-      // } else if (progress >= 0.36 && progress < 0.59) {
-      // } else if (progress >= 0.59 && progress < 0.95) {
-      //   $(".marquee-item .uppercase").text("eco");
-      //   $(".marquee").css(
-      //     "background-image",
-      //     "linear-gradient(0deg, #00D543, #0CACFF)"
-      //   );
-      // } else if (progress >= 0.95) {
-      //   $(".marquee-item .uppercase").text("with");
-      //   $(".marquee").css(
-      //     "background-image",
-      //     "linear-gradient(0deg, #EE7F31, #FF3973)"
-      //   );
-      // }
+      changeHeaderStyle(progress);
     },
   });
 
-  const setHeaderStyle = (isDark) => {
-    if (isDark) {
+  const sideMarquee = gsap.to(".marquee-item", {
+    xPercent: 10,
+    repeat: -1,
+    duration: 4,
+    ease: "none",
+    paused: true,
+  });
+  const setHeaderStyle = (bgColor) => {
+    if (bgColor === "transparent") {
       $("#header").addClass("dark");
       $("#header").css({
         "background-image":
           "linear-gradient(180deg,rgba(0, 0, 0, 0.1),rgba(0, 0, 0, 0.1))",
         "background-color": "transparent",
       });
-    } else {
+    } else if (bgColor === "white") {
+      $("#header").addClass("dark");
+      $("#header").css({
+        "background-image": "none",
+        "background-color": "#fff",
+      });
+    } else if (bgColor === "black") {
       $("#header").removeClass("dark");
       $("#header").css({
         "background-image": "none",
         "background-color": "#000",
-      });
-    }
-  };
-  const animateMarquee = (position) => {
-    if (position === "fixed") {
-      $(".marquee").css("position", "fixed");
-      gsap.to(".flex.slide .marquee", {
-        xPercent: 0,
-        onComplete: () => {
-          sideMarquee.play();
-        },
-      });
-    } else if (position === "static") {
-      gsap.to(".flex.slide .marquee", {
-        xPercent: -100,
-        onComplete: () => {
-          sideMarquee.pause();
-          $(".marquee").css("position", "static");
-        },
       });
     }
   };
@@ -491,12 +469,21 @@ function sectionChallenge() {
   });
 }
 function sectionContact() {
-  // sc-contact
+  gsap.set(".sc-contact .group-content", { y: 194 });
+  gsap.to(".sc-contact .group-content", {
+    y: 0,
+    scrollTrigger: {
+      trigger: ".sc-contact",
+      start: "bottom 86%",
+      end: "bottom+=160px bottom",
+      scrub: 1,
+    },
+  });
 }
 
 $(function () {
-  // sectionIntro();
-  // sectionInnovation();
+  sectionIntro();
+  sectionInnovation();
   sectionMainSlide();
   sectionChallenge();
   sectionContact();
